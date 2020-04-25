31918f9dc99ad061d0c576b06d1ec0376a3553c6
Martin Zalazar <mzalazar.linux@gmail.com>
2020-04-25 00:30:33 -0300

    build

---
+++ build/precache-manifest.ce6f3b55e882f0a56b22e911924047f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfec95c6bc5b258e6a2b54dfcda81f0f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1881055fe821ba2bf4d6",
+    "url": "/monsters-rolodex/static/css/main.3375ed60.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1881055fe821ba2bf4d6",
+    "url": "/monsters-rolodex/static/js/main.c7e49437.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
